e67c02dc346d56a5949760f3161a93b4535e2c25
felipe <felipekurosaki123@gmail.com>
2022-06-08 11:20:58 -0300

    update and create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PublicHeader } from './pages/Headers/PublicHeader';
+import { Register } from './pages/Form/Register';
+import { Login } from './pages/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/"  index={false} element={<PublicHeader />} />
+          <Route path="/login"  element={<Login />} />
+          <Route path="/register"  element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
