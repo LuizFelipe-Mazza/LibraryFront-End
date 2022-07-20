803c855900728669df4f6e05e567eb522e679124
felipe <felipekurosaki123@gmail.com>
2022-07-20 16:41:05 -0300

    fix: upgrade in routes

--- src/Services/registerProvider.ts
+++ src/Services/registerProvider.ts
@@ -4,8 +4,8 @@ import api from './axios'
 export async function saveProvider(id:number, name:string, ){
 return await api.post('/register',{id, name})
 }
-export async function showProvider(){
-    return await api.get('/provider/paginate')
+export async function showProvider(page:number, pageSize:number){
+    return await api.get(`/provider/paginate/${page}, ${pageSize}`)
 }
 export async function Provider(id:number){
     return await api.get(`/showprovider/${id}`)
