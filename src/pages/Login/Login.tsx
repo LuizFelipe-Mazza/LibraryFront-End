506523d633cf5930dff5e16f90882931bddccd70
felipe <felipekurosaki123@gmail.com>
2022-07-26 16:55:16 -0300

    doc: new changes to interface visual login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,8 @@
-import { Button, Input } from '../../components'
+import { Button} from '../../components'
 import { GoogleLogin } from 'react-google-login'
 import { Context } from '../../context/Context'
 
+import MenuBookRoundedIcon from '@mui/icons-material/MenuBookRounded';
 import 'sweetalert2/src/sweetalert2.scss'
 import './login.scss'
 
@@ -9,44 +10,54 @@ export function Login() {
   const { email, setEmail, password, setPassword, SignIn } = Context()
   return (
     <>
-      <form action="" className="formLogin">
-        <div className="containerSigIn">
-          <h1>
-            Log<span>In</span>
-            <small>Library</small>
-          </h1>
+      <div className="loginContainer">
+        <h1>
+          Log<span>in</span>
+          <small>Library <MenuBookRoundedIcon className='iconLogin'/> </small>
+        </h1>
+        <div className="bgLogin">
 
-          <div className="emailField">
-            <Input
-              name="Insira o E-mail"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              required
-            />
+          <div className="email">
+            <input type="text" 
+            name='email' 
+            placeholder="Email" 
+            value={email}
+            onChange={event => setEmail(event.target.value)}/>
           </div>
 
-          <div className="passwordField">
-            <Input
-              type="password"
-              name="Insira a Senha"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              required
-            />
+          <div className="password">
+            <input type="password" 
+            name='password' 
+            placeholder="Senha" 
+            value={password}
+            onChange={event => setPassword(event.target.value)}/>
+          </div>
+          
+          <div className="links">
 
-            <div className="links">
-              <a href=".">Esqueci a Senha</a>
-              <a href="/register">Cadastrar</a>
+            <div className="forgot">
+              <a href="//">Esqueci a Senha</a>
             </div>
-            <Button onClick={SignIn}>Entrar</Button>
-            <GoogleLogin
-              className="btnGoogle"
-              clientId="512809359891-i53ug9f9k324m38d1tlkai6jb9ttuktt.apps.googleusercontent.com"
-              buttonText="Continuar com Google"
-            />
+
+            <div className="create">
+              <a href="/createaccount">Criar Conta</a>
+            </div>
+
           </div>
+
+          <Button onClick={SignIn}>Entrar</Button>
+          
+          <div className="or">
+          <h4>Ou</h4>
+          </div>
+
+          <GoogleLogin
+            className="btnGoogle"
+            clientId="512809359891-i53ug9f9k324m38d1tlkai6jb9ttuktt.apps.googleusercontent.com"
+            buttonText="Continuar com Google"/>
+
         </div>
-      </form>
+      </div>
     </>
   )
 }
