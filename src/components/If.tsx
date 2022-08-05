f36c16abade4e72a77bc6b706ffac6f961d6e930
felipe <felipekurosaki123@gmail.com>
2022-08-04 22:51:05 -0300

    fix: ...

--- src/components/If.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react'
-type Props = {
-  children: ReactNode
-  condition: boolean
-}
-export function If(props: Props) {
-  return props.condition ? props.children : null
-}
