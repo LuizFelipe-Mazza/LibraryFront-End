89f3f18f901a24515df33b923034e793db382bd0
felipe <felipekurosaki123@gmail.com>
2022-08-16 09:39:47 -0300

    fix: new route and new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { ProviderFormContainer } from './pages/containers/providerFormContainer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AuthContextProvider } from './context/AuthContext';
-import { RegisterUser } from './pages/RegisterUser/RegisterUser';
-import { Presentation } from './pages/Presentation/Presentation';
-import { PublicPage } from './pages/Screen/Public/PublicPage';
-import { NewBook } from './pages/RegisterBook/NewBook';
-import { About } from './pages/About/About';
-import { Login } from './pages/Login/Login';
+import { ProviderFormContainer } from './pages/containers/providerFormContainer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext'
+import { RegisterUser } from './pages/RegisterUser/RegisterUser'
+import { Presentation } from './pages/Presentation/Presentation'
+import { PublicPage } from './pages/Screen/Public/PublicPage'
+import { NewBook } from './pages/RegisterBook/NewBook'
+import { About } from './pages/About/About'
+import { Login } from './pages/Login/Login'
 
-import  ProviderList  from './pages/containers/ProviderList';
-import { Cart } from './pages/Cart/Cart';
-import { MyCart } from './components/Modal/MyCart';
+import ProviderList from './pages/containers/ProviderList'
+import { FinalCart } from './pages/FinalCart/FinalCart'
+import { Cart } from './pages/Cart/Cart'
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+      <BrowserRouter>
         <AuthContextProvider>
-        <Routes>
-          <Route path="/"  children element={<PublicPage />} />
-          <Route path="/welcome" children element={<Presentation/>} />
-          <Route path="/aboutlibrary" element={<About/>} />
-          <Route path="/login"  element={<Login />} />
-          <Route path="/registeruser"  element={<RegisterUser />} />
-          <Route path="/update/:id" element={<ProviderFormContainer/>} />
-          <Route path='/table' element={<ProviderList/>} />
-          <Route path='/newbook'element={<NewBook/>} />
-           <Route path='/book/:product_code'element={<Cart/>}/> 
-          <Route path='/mycart'element={<MyCart/>}/> 
-        </Routes>
+          <Routes>
+            <Route path="/" children element={<PublicPage />} />
+            <Route path="/welcome" children element={<Presentation />} />
+            <Route path="/aboutlibrary" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registeruser" element={<RegisterUser />} />
+            <Route path="/update/:id" element={<ProviderFormContainer />} />
+            <Route path="/table" element={<ProviderList />} />
+            <Route path="/newbook" element={<NewBook />} />
+            <Route path="/book/:product_code" element={<Cart />} />
+            <Route path="/mycart"element={<FinalCart/>}/>
+          </Routes>
         </AuthContextProvider>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
